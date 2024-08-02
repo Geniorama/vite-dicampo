b7601ab42ad28f67d824bfcc9288b3d00d039e59
angelpublicista <angelpublicista.1@gmail.com>
2024-08-01 22:41:27 -0500

    fix: enviroment variables verify

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,8 +1,15 @@
-import { createClient } from "contentful";
+import { createClient } from 'contentful';
+
+const space = import.meta.env.VITE_CONTENTFUL_SPACE_ID;
+const accessToken = import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN;
+
+if (!space || !accessToken) {
+  throw new Error("Missing VITE_CONTENTFUL_SPACE_ID or VITE_CONTENTFUL_ACCESS_TOKEN in environment variables");
+}
 
 const client = createClient({
-    space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
-    accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,
-  });
-  
+  space: space,
+  accessToken: accessToken,
+});
+
 export default client;
